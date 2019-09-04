bd2392766744d04d2c5a6153b6a501e227fdbc18
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-09-04 20:55:50 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="row">
-      <div className="col-12 text-center">
-        <h1>Hello world</h1>
+    <React.Fragment>
+      <Navbar />
+      <div className="container pt-5">
+        <Route exact path="/" component={Home} />
+        <Route path="/login/" component={Login} />
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
