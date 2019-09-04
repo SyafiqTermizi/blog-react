945f46d905d4738c037182c80f0c6bf14020d789
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-09-04 20:55:41 +0800

    add home component

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Home = () => (
+    <div className="row">
+        <div className="col-12 text-center">
+            <h1>Home</h1>
+        </div>
+    </div>
+);
+
+export default Home;
