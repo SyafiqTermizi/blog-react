946a3de16d99d247961fefcdabc3fa33f5b74486
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-10-09 22:56:10 +0800

    lazy load routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import Form from './pages/Form';
-
 import Navbar from './components/Navbar';
+import Loading from './components/Loading';
+
+const Login = lazy(() => import('./pages/Login'));
+const Home = lazy(() => import('./pages/Home'));
+const Detail = lazy(() => import('./pages/Detail'));
+const Form = lazy(() => import('./pages/Form'));
 
 const App = props => (
   <React.Fragment>
     <Navbar />
-    <div className="container pt-5">
-      <Route exact path="/" component={Home} />
-      <Route path="/detail/:id" component={Detail} />
-      <Route path="/login/" component={Login} />
-      <Route
-        path="/create/"
-        render={ () => props.token ? <Form token={props.token} />:<Login />}
-      />
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className="container pt-5">
+        <Route exact path="/" component={Home} />
+        <Route path="/detail/:id" component={Detail} />
+        <Route path="/login/" component={Login} />
+        <Route
+          path="/create/"
+          render={ () => props.token ? <Form token={props.token} />:<Login />}
+        />
+      </div>
+    </Suspense>
   </React.Fragment>
 )
 
